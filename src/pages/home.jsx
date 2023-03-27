5c110bb3ad6f5adaedb2b0aa418c5875796ce37c
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2023-03-27 23:21:09 +0200

    coding

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
+import img2 from "../assets/image2.png";
 import img from "../assets/image.png";
 
 const Home = () => {
@@ -10,7 +11,21 @@ const Home = () => {
     <>
       <Header/>
 
-      <img src={img} alt="" />
+      <div className="main">
+        <div className="landing-page">
+          <img src={img2} alt="" />
+          <div className="txt">
+            <h1>جمعية التيسير
+            <br/> لتحفيظ القرآن الكريم</h1>
+          
+            <h3>جمعية لتحفيظ القرآن الكريم بالقراءات العشر للأطفال (للصبيان ، و البنات).</h3>
+          </div>
+        </div>
+        <div className="first">
+          <h1>خيرُكُم مَن تعلَّمَ القرآنَ وعلَّمَهُ</h1>
+          <img src={img} alt="" />
+        </div>
+      </div>
 
       <Footer/>
     </>
